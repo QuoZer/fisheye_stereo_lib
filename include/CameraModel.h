7e0ee7358b318ecbb6e815a1827db4601fcbd1a5
QuoZer <57410392+QuoZer@users.noreply.github.com>
2023-02-12 17:01:11 +0200

    camera model definitions documented

--- include/CameraModel.h
+++ include/CameraModel.h
@@ -12,6 +12,10 @@
 *  setResolution 
 */
 
+/// <summary>
+/// A parent class for all the camera models. Inherit overriding virtual functions 'projectWorldToPixel' and 'projectPixelToWorld'
+/// to add your own camera models
+/// </summary>
 class CameraModel
 {
 public:	///* Parameters *///
@@ -32,28 +36,55 @@ public:	///* Data *///
 
 public:	///* Internal functions *///
 	/* Tools */
-	// converting corner coordinates to the center ones
+
+
+	/// <summary>
+	/// Transforms a pixel from opencv corner reference frame to the central one
+	/// </summary>
+	/// <param name="cornerPixel"> Pixel that needs to be transformed </param>
+	/// <param name="imagesize"> Image size </param>
 	void toCenter(cv::Point& cornerPixel, cv::Size imagesize);
-	// converting center coordinates to the corner ones
+
+	/// <summary>
+	/// Transforms a pixel from central reference frame to the opencv one
+	/// </summary>
+	/// <param name="centerPixel"> Pixel that needs to be transformed </param>
+	/// <param name="imagesize"> Image size </param>
 	void toCorner(cv::Point& centerPixel, cv::Size imagesize);
 
 public:		///* Settings *///
 	CameraModel();
+
+	/// <summary>
+	/// Set parameters of the fisheye camera. Only resolution matters at this point
+	/// </summary>
+	/// <param name="origImageSize"> Image resolution </param>
 	void setCamParams(cv::Size origImageSize);
+
+	/// <summary>
+	/// Set camera position and orientation on your robot/setup. Keep in mind that all cameras should use common reference frame
+	/// </summary>
+	/// <param name="pos"> Position in xyz coordinates </param>
+	/// <param name="rot"> Orientation in quaternion </param>
 	void setExtrinsics(cv::Vec3d pos, cv::Vec4d rot);
 
+	// trying to use one agnostic function to set parameters no matter the parameters
 	void setIntrinsics(DistortionParams params);
+
 	/* Projection functions */
 
-	/*
-	\brief Takes a 3D point in camera coordinates and projects it into fisheye image coordinates
-	
-	*/
+	/// <summary>
+	/// Virtual. Takes a 3D point in camera coordinates and projects it into fisheye image coordinates
+	/// </summary>
+	/// <param name="worldPoint"> 3D point in camera coordinates </param>
+	/// <returns> 2D point in image plane </returns>
 	virtual cv::Point2d projectWorldToPixel(cv::Mat worldPoint) = 0; 		// idk it doesnt want to return null
-	/*
-	\brief Takes a point in image (central coordinates) and projects it into camera coordinates
 
-	*/
+	/// <summary>
+	///  Virtual. Takes a point in image (central coordinates) and projects it into camera coordinates. 
+	/// </summary>
+	/// <param name="pixel"> 2D point in image plane </param>
+	/// <returns> 3D point in camera coordinates </returns>
 	virtual cv::Mat projectPixelToWorld(cv::Point pixel) = 0;
 
 };
--- include/models.h
+++ include/models.h
@@ -1,10 +1,16 @@
 #pragma once 
 #include "CameraModel.h"
 
+/*
+	The file for defining camera models. Write or #include yours here
+*/
 
+/// <summary>
+/// Basic pinhole model
+/// </summary>
 class PinholeModel : public CameraModel
 {
-	/* Parameters as in parent */
+	/* Parameters as in the parent */
 
 public:	///* Projection functions *///
 	PinholeModel();
@@ -13,7 +19,12 @@ public:	///* Projection functions *///
 	void setIntrinsics(cv::Size newsize, float wideFov);
 };
 
-
+/// <summary>
+/// Scaramuzza camera model from the MATLAB camera calibration toolbox. 
+/// Distortion parameters: 4 coefficients and a scaling factor.
+/// Projection parameters: image centre coordinates and a stretch matrix.
+/// More at: https://www.mathworks.com/help/vision/ug/fisheye-calibration-basics.html
+/// </summary>
 class ScaramuzzaModel : public CameraModel
 {
 private:	///* Intrinsics *///
@@ -27,10 +38,20 @@ public:	///* Projection functions *///
 	cv::Mat projectPixelToWorld(cv::Point pixel);
 
 public:
+	/// <summary>
+	/// Specify camera intrinsic parameters according to the model. Can be obtained from e.g. MATLAB calibration toolbox. 
+	/// </summary>
+	/// <param name="coeffs"> 4 polynomial coefficients as a list in '{}' brackets.  </param>
+	/// <param name="lambda"> Lambda scaling factor </param>
+	/// <param name="centerOffset"> Image centre </param>
+	/// <param name="stretchMatrix"> 2x2 Stretch matrix </param>
 	void setIntrinsics(std::initializer_list<double> coeffs, double lambda,
 		cv::Vec2d centerOffset, cv::Matx22d stretchMatrix);
 };
 
+/// <summary>
+/// 
+/// </summary>
 class AtanModel : public CameraModel
 {
 private:	///* Intrinsics *///
@@ -64,6 +85,12 @@ public:
 
 };
 
+/// <summary>
+/// Kannala-Brandt camera model. Can be calibrated in OpenCV and CamOdoCal libraries
+/// Distortion parameters: 4 coefficients.
+/// Projection parameters: image centre coordinates and a stretch matrix.
+/// More at:
+/// </summary>
 class KBModel : public CameraModel
 {
 private:
@@ -76,15 +103,28 @@ public:
 	KBModel(std::initializer_list<double> coeffs,
 		cv::Vec2d centerOffset, cv::Matx22d stretchMatrix);
 
+	/// <summary>
+	/// Specify camera intrinsic parameters according to the model.
+	/// </summary>
+	/// <param name="coeffs"> 4 polynomial coefficients as a list in '{}' brackets. </param>
+	/// <param name="centerOffset"> Image centre </param>
+	/// <param name="stretchMatrix"> 2x2 Stretch matrix </param>
 	void setIntrinsics(std::initializer_list<double> coeffs,
 		cv::Vec2d centerOffset, cv::Matx22d stretchMatrix);
 
 	cv::Point2d projectWorldToPixel(cv::Mat worldPoint);
 	cv::Mat projectPixelToWorld(cv::Point pixel);
+
+	// reference
 	void backprojectSymmetric(cv::Point pxl, double& theta, double& phi);
 };
 
-
+/// <summary>
+/// Christopher Mei camera model. Can be calibrated using CamOdoCal library
+/// Distortion parameters: 5 coefficients.
+/// Projection parameters: image centre coordinates and a stretch matrix.
+/// More at:
+/// </summary>
 class MeiModel : public CameraModel
 {
 private:
@@ -101,7 +141,12 @@ public:
 	cv::Mat projectPixelToWorld(cv::Point pixel);
 };
 
-
+/// <summary>
+/// Double sphere model (doesn't seem to work at the moment)
+/// Distortion parameters: 2 coefficients.
+/// Projection parameters: image centre coordinates and a stretch matrix.
+/// More at:
+/// </summary>
 class DSModel : public CameraModel
 {
 private:
