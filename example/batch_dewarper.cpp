da29f6ca8ac3d63e424573dae46d5d0c3c05c358
QuoZer <57410392+QuoZer@users.noreply.github.com>
2024-01-26 16:04:49 +0100

    dataset parsing

---
+++ example/batch_dewarper.cpp
@@ -0,0 +1,136 @@
+//#include "opencv2/ccalib/omnidir.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/calib3d.hpp"
+#include "opencv2/highgui.hpp"
+#include <opencv2/videoio.hpp>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+#include <time.h>
+#include <map>
+#include <cstdarg>
+#include "SurroundSystem.hpp"
+#include "Utils.hpp"
+
+using namespace cv;
+using namespace std;
+
+int resavePLY(string inputFileName, string outputFileName)
+{
+    // Open the input file
+	std::ifstream inputFile(inputFileName, std::ios::binary);
+    if (!inputFile) {
+		std::cerr << "Error: Unable to open input file: " << inputFileName << std::endl;
+		return 1;
+	}
+
+	// Open the output file
+	std::ofstream outputFile(outputFileName, std::ios::binary);
+    if (!outputFile) {
+		std::cerr << "Error: Unable to open output file: " << outputFileName << std::endl;
+		return 1;
+	}
+
+	// Read data from the input file and write it to the output file
+	outputFile << inputFile.rdbuf();
+
+	// Close the files
+	inputFile.close();
+	outputFile.close();
+}
+
+// Function to get a number of files in a directory
+int getNumFiles(string path)
+{
+	int num_files = 0;
+	for (auto const& entry : filesystem::directory_iterator(path))
+		num_files++;
+	return num_files;
+}
+
+
+int main(int argc, char** argv)
+{ 
+    string base_path = "C:/Users/Matvey/Repos/fisheye_stereo/data/ELTE2/120deg/";
+    string param_path = "C:/Users/Matvey/Repos/fisheye_stereo/fy_lib_source/config/elte_params_120deg.xml";
+
+    // indices of the measurement images to be dewarped
+    std::vector<int> indicies = {4, 6, 11, 16, 21, 26, 31, 36, 41, 45}; // 120 deg
+    // 
+    int num_distances = getNumFiles(base_path + "calibration") / 2;
+
+    bool swap_lr = true;
+
+    SurroundSystem SS("elte");
+    SS.readSystemParams(param_path);
+    SS.prepareLUTs(false);
+    Size newSize(540, 540);          // determines the size of the output image
+
+    // loop over all measurements 
+    int new_index = 1;
+    for (int index : indicies)
+    {
+        string left_path, right_path, scan_path, new_scan_path;
+        if (swap_lr)
+        {
+			left_path = base_path + "measurements/Dev0_Image_w1920_h1200_fn" + to_string(index) + ".bmp";
+			right_path = base_path + "measurements/Dev1_Image_w1920_h1200_fn" + to_string(index) + ".bmp";
+		}
+        else
+        {
+            left_path = base_path + "measurements/Dev1_Image_w1920_h1200_fn" + to_string(index) + ".bmp";
+			right_path = base_path + "measurements/Dev0_Image_w1920_h1200_fn" + to_string(index) + ".bmp";
+        }
+        Mat left = imread(left_path, -1);
+        Mat right = imread(right_path, -1);
+        Size origSize = left.size();
+
+        // read pointcloud 
+        scan_path = base_path + "scans/test_fn" + to_string(index) + ".ply";
+        new_scan_path = base_path + "scans/left_d" + to_string(new_index) + "_dewarped.ply";
+        // resave under different name
+        resavePLY(scan_path, new_scan_path);
+        
+        // remove distortion
+        Mat combinedRemap1(Size(newSize.width*2, newSize.height), CV_8UC3, Scalar(0, 0, 0));
+        SS.getImage(0, SurroundSystem::RECTIFIED, left, right, combinedRemap1);
+
+        imwrite(base_path +"left/left_d" + to_string(new_index) + ".png", combinedRemap1(Rect(0, 0, newSize.width, newSize.height)));
+        imwrite(base_path +"right/right_d" + to_string(new_index) + ".png", combinedRemap1(Rect(newSize.width, 0, newSize.width, newSize.height)));
+
+        cout << new_index <<  " Index done" << endl;
+        new_index++;
+    }
+
+    // loop over all calibration images
+    for (int i = 1; i <= num_distances; i++)
+    {
+        string left_path, right_path, scan_path, new_scan_path;
+        if (swap_lr)
+        {
+            left_path = base_path + "calibration/Dev0_Image_w1920_h1200_fn" + to_string(i) + ".bmp";
+            right_path = base_path + "calibration/Dev1_Image_w1920_h1200_fn" + to_string(i) + ".bmp";
+        }
+        else
+        {
+            left_path = base_path + "calibration/Dev1_Image_w1920_h1200_fn" + to_string(i) + ".bmp";
+            right_path = base_path + "calibration/Dev0_Image_w1920_h1200_fn" + to_string(i) + ".bmp";
+        }
+        Mat left = imread(left_path, -1);
+        Mat right = imread(right_path, -1);
+        Size origSize = left.size();
+
+        // remove distortion
+        Mat combinedRemap1(Size(newSize.width * 2, newSize.height), CV_8UC3, Scalar(0, 0, 0));
+        SS.getImage(0, SurroundSystem::RECTIFIED, left, right, combinedRemap1);
+
+        imwrite(base_path + "left/left_fn" + to_string(i) + ".png", combinedRemap1(Rect(0, 0, newSize.width, newSize.height)));
+        imwrite(base_path + "right/right_fn" + to_string(i) + ".png", combinedRemap1(Rect(newSize.width, 0, newSize.width, newSize.height)));
+
+        cout << i << " Index done" << endl;
+	}
+
+}
---
+++ example/demo.cpp
@@ -0,0 +1,68 @@
+//#include "opencv2/ccalib/omnidir.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/calib3d.hpp"
+#include "opencv2/highgui.hpp"
+#include <opencv2/videoio.hpp>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <map>
+#include <cstdarg>
+#include "SurroundSystem.hpp"
+#include "Utils.hpp"
+
+using namespace cv;
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+    // test image path
+    string left_path = "C:/Users/Matvey/Repos/fisheye_stereo/data/ELTE/90deg/Dev1_Image_w1920_h1200_fn6.bmp";
+    string right_path = "C:/Users/Matvey/Repos/fisheye_stereo/data/ELTE/90deg/Dev0_Image_w1920_h1200_fn6.bmp";
+    string write_path = ".";
+    // read the image and separate into two 
+    Mat left = imread(left_path, -1);
+    Mat right = imread(right_path, -1);
+    // display input
+    ShowManyImages("Originals", 2, left, right);
+    waitKey(15);
+
+    Size origSize(1920, 1200);       //left.size();
+    Size newSize(540, 540);          // determines the size of the output image
+    
+/*  1. Create the stereo system object    */ 
+    SurroundSystem SS("elte");
+/*  2. Describe your system. */
+    SS.readSystemParams("C:/Users/Matvey/Repos/fisheye_stereo/fy_lib_source/elte_params.xml");
+
+/*  3. Compute look-up tables  */
+    SS.prepareLUTs(false);
+
+/*  3.1 OR load already filled ones (WIP) */
+    //SS.loadLUTs("svs");
+	
+/*  4. Get images  */
+    Mat combinedRemap1(Size(newSize.width*2, newSize.height), CV_8UC3, Scalar(0, 0, 0));
+    Mat disparity1(newSize, CV_32F, Scalar(0, 0, 0));
+    
+    SS.getImage(0, SurroundSystem::DISPARITY, left, right, disparity1);
+    SS.getImage(0, SurroundSystem::RECTIFIED, left, right, combinedRemap1);
+    
+
+    // Save combinedRemap
+    //imwrite("./combinedRemap1.png", combinedRemap1);
+
+/*  5. See the results  */
+    imshow("Remapped", combinedRemap1);
+    //ShowManyImages("Remapped", 2, combinedRemap1, combinedRemap2);
+    waitKey(0);        
+
+    /*  6. Save the results  */
+    imwrite("./left_fn4.png", combinedRemap1(Rect(0, 0, newSize.width, newSize.height)));
+    imwrite("./right_fn4.png", combinedRemap1(Rect(newSize.width, 0, newSize.width, newSize.height)));
+
+
+}
